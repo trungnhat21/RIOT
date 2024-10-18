3a5ee7fa5b86cb1ff5415cd954143e2ae1940a98
Ben <ben84@example.com>
2024-10-18 20:09:17 +0700

    upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//  var fullName = "Nguyễn Nhật Trung";
+// var age = 20;
+// alert(fullName);
+// alert(age);
+// console.error(fullName);
+// confirm("Xác Nhận");
+// prompt("Xác nhận");
+// setTimeout(function() {
+//     alert("Thông Báo")
+// }, 1000)
+// setInterval(function() {
+//     console.log("Đây là log" + Math.random())
+// }, 1000)
+// var a = 2 + 4;
+// console.log(a)
+// var a = 2;
+// var b = 4;
+// if (a > b) {
+//     alert("False");
+// } else {
+//     alert("True");
+// }
